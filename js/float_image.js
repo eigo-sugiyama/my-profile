54858ed26a828a2fdd8174a5d33287f08398c53f
eigosugiyama <eigo.sugiyama0403@gmail.com>
2024-08-13 22:51:31 +0900

    gsap_edit_mv_image

--- js/float_image.js
+++ js/float_image.js
@@ -1,7 +1,7 @@
 
 // GSAPで画像を上下にフワフワ動かす
   gsap.to('.floating-image', {
-      y: 30, // 上下に移動する距離
+      y: 3, // 上下に移動する距離
       duration: 2, // アニメーションの時間
       ease: "power1.inOut",
       yoyo: true, // アニメーションを往復させる
